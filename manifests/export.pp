d43b4a349a9909d7f66b187ad1efbbfa5b2321b6
Jordan Conway <jconway@linuxfoundation.org>
2016-05-06 14:46:51 -0400

    Added 7 case for lsbmajdistrelease

--- manifests/export.pp
+++ manifests/export.pp
@@ -37,7 +37,7 @@ define rsyncd::export (
           /RedHat|CentOS/: {
             case $::lsbmajdistrelease {
 
-              '4','5','6': { }
+              '4','5','6','7': { }
 
               default: {
                 augeas { "setup rsyncd munge symlinks ${name}":
